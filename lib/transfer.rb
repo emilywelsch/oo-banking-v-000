25cf76ad0937cb3c64eb227df0a551ede81f3224
Emily Welsch <emwelsch@gmail.com>
2018-05-30 16:06:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,9 +13,10 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance >= @amount && @status = "pending"
+    if @sender.balance >= @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
+      @status = "complete"
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
