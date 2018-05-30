5ddb0ce220377eeb4a6926c6420e05ff555cfb8b
Emily Welsch <emwelsch@gmail.com>
2018-05-30 15:42:27 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,24 +8,25 @@ class BankAccount
     status == "open"
   end
 
-    def deposit(amount)
-      account += amount
-    end
-
-    def display_balance
-      balance
-    end
-
-    def valid?(status)
-      status == "open" if balance >= 0
-      else status == "closed"
-    end
-
-    def close_account
-      account.clear
-    end
-    
+  def deposit(amount)
+    account += amount
   end
+
+  def display_balance
+    balance
+  end
+
+  def valid?(status)
+    status == "open" if balance >= 0
+    else status == "closed"
+  end
+
+  def close_account
+    account.clear
+  end
+
+    
+end
 
 
 
