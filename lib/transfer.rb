0edd86a666155a9144189dae3423989be1ad5720
Emily Welsch <emwelsch@gmail.com>
2018-05-30 15:33:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,20 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver
+
+  def initialize(transfer)
+    @transfer = transfer
+    status = "pending"
+    transfer_amount = "transfer_amount"
+  end
+
+  def valid?
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
+
+
 end
